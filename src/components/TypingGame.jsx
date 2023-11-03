0a7dfec9d045fd93369c02135a40bd8fd2d68108
Ayo Omotosho <aomotosho4@gmail.com>
2023-11-03 12:24:39 -0500

    add random word gen + focus input on click

--- src/components/TypingGame.jsx
+++ src/components/TypingGame.jsx
@@ -1,74 +1,77 @@
 import React from "react";
-import { useState } from "react";
+import { commonWords } from "./CommonWords";
+import { useState, useEffect } from "react";
 
 export const TypingGame = () => {
   const [input, setInput] = useState("");
-  const quote = "Hello this is a sentence that means absolutely nothing";
-  const newQuote = quote.split("");
+  const [displayPhrase, setDisplayPhrase] = useState([]);
+  const [isFocused, setIsFocused] = useState(false);
+  const element = document.querySelectorAll("span");
 
-  const test = newQuote.map((char, index) => {
+  const test2 = displayPhrase.map((char, index) => {
     return <span key={index}>{char}</span>;
   });
 
-  const element = document.querySelectorAll("span");
+  const generatePhrase = () => {
+    let arr = [];
+    let i = 0;
 
-  /*
-   *
+    while (i < 30) {
+      const randomIndex = Math.floor(Math.random() * commonWords.length);
+      const randomWord = commonWords[randomIndex];
+      const word = randomWord.split("");
+      word.push(" ");
+      arr = [...arr, ...word];
 
-const handleChange = (e) => {
-  const value = e.target.value;
-  const currentChar = value[value.length - 1];
-  setInput(e.target.value);
-
-  element.forEach((span, index) => {
-    if (index === value.length - 1) {
-      if (currentChar === newQuote[value.length - 1]) {
-        span.classList.add("correct");
-        span.classList.remove("incorrect");
-      } else {
-        span.classList.remove("correct");
-        span.classList.add("incorrect");
-      }
-    } else if (index < value.length - 1) {
-      span.classList.add("correct");
-      span.classList.remove("incorrect");
-    } else {
-      span.classList.remove("correct");
-      span.classList.remove("incorrect");
+      i++;
     }
-  });
-};
-   * */
+    console.log(arr);
+    setDisplayPhrase(arr);
+  };
 
   const handleChange = (e) => {
     const value = e.target.value;
-    const currentChar = value[value.length - 1];
+    const arrayValue = value.split("");
     setInput(e.target.value);
 
-    element.forEach((span, index) => {
-      if (index === value.length - 1) {
-        if (currentChar === newQuote[value.length - 1]) {
-          span.classList.add("correct");
-          span.classList.remove("incorrect");
-        } else {
-          span.classList.remove("correct");
-          span.classList.add("incorrect");
-        }
-      } else if (index < value.length - 1) {
-        span.classList.add("correct");
-        span.classList.remove("incorrect");
+    element.forEach((charSpan, index) => {
+      const currentChar = arrayValue[index];
+      console.log(currentChar, charSpan.innerText);
+      if (currentChar == null) {
+        charSpan.classList.remove("correct");
+        charSpan.classList.remove("incorrect");
+      } else if (currentChar === charSpan.innerText) {
+        charSpan.classList.add("correct");
+        charSpan.classList.remove("incorrect");
       } else {
-        span.classList.remove("correct");
-        span.classList.remove("incorrect");
+        charSpan.classList.add("incorrect");
+        charSpan.classList.remove("correct");
       }
     });
   };
 
+  const focusInput = () => {
+    const input = document.querySelector("input");
+    input.focus();
+    setIsFocused(true);
+  };
+
+  useEffect(() => {
+    generatePhrase();
+  }, []);
+
   return (
     <div>
-      <div className="game-container">
-        <input value={input} onChange={(e) => handleChange(e)} />
-        <div className="quote">{test}</div>
+      <h1>geckotype</h1>
+      <div className="game-container ">
+        {isFocused ? <></> : <p>Click here or start typing to focus</p>}
+        <div
+          className={`${isFocused ? "quote" : "quote unfocused"}`}
+          onClick={focusInput}
+        >
+          <input value={input} onChange={(e) => handleChange(e)} autoFocus />
+          <div className="quote">{test2}</div>
+        </div>
       </div>
     </div>
   );

7e8e451be65986d7cc76b05977bd26d1a0e21a52
Ayo Omotosho <aomotosho4@gmail.com>
2023-11-30 23:32:32 -0600

    feat: update stats page:

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -20,24 +20,41 @@ export const Stats = ({
   };
 
   let temp = numCorrect / 5;
+  //wpm should be total correct chars in total correct words / 5 normalized to 1 minute
   let correctWpm = Math.floor(temp / (totalTime.current / 60));
   let rawWpm = Math.floor(
     (numCorrect + numIncorrect) / 5 / (totalTime.current / 60),
   );
 
   return (
-    <div className="stats-container">
-      <h2 className="wpm">WPM: {correctWpm}</h2>
-      <h2 className="acc">
-        Accuracy: {Math.floor((numCorrect / totalChars) * 100)}%
-      </h2>
-      <h2> Raw: {rawWpm}</h2>
-      <h2>
-        Characters {numCorrect}/{numIncorrect}
-      </h2>
-      <h2> Total Time {totalTime.current}s</h2>
-
-      <IoIosArrowForward className="arrow" onClick={handleRestart} />
-    </div>
+    <>
+      <div className="stats-container">
+        <div className="stat-line">
+          <p className="">wpm</p>
+          <h2 className="result">{correctWpm}</h2>
+        </div>
+        <div className="stat-line">
+          <p className="acc">accuracy</p>
+          <h2 className="result">
+            {Math.floor((numCorrect / totalChars) * 100)}%
+          </h2>
+        </div>
+        <div className="stat-line">
+          <p>raw</p>
+          <h2 className="result">{rawWpm}</h2>
+        </div>
+        <div className="stat-line">
+          <p>characters</p>
+          <h2 className="result">
+            {numCorrect}/{numIncorrect}
+          </h2>
+        </div>
+        <div className="stat-line">
+          <p>time</p>
+          <h2 className="result">{totalTime.current}s</h2>
+        </div>
+      </div>
+      <IoIosArrowForward className="arrow" size={20} onClick={handleRestart} />
+    </>
   );
 };
